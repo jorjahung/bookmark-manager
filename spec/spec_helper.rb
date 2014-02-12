22cef33536efc73c4747fce95d672bd0f6135804
Jorja Hung <jorjahung@gmail.com>
2014-02-12 14:10:20 +0000

    created controllers and moved them to /app/controllers; moved models from /lib to /app/models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RACK_ENV"] = 'test' # because we need to know what database to work with
 require './app/server' 
 require 'database_cleaner'
-require 'sinatra'
 require 'capybara/rspec'
 
 Capybara.app = Sinatra::Application
