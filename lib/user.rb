178237c0b25693bd679dcf1a6f6c2ced441427c1
Jorja Hung <jorjahung@gmail.com>
2014-02-12 11:22:19 +0000

    created SessionHelpers module for rspec tests

--- lib/user.rb
+++ lib/user.rb
@@ -33,5 +33,23 @@ class User
   # http://datamapper.org/docs/validations.html
   validates_confirmation_of :password, :message => "Your passwords don't match"
   validates_uniqueness_of :email
+
+  def self.authenticate(email, password)
+    user = first(:email => email)
+    # if this user signing in exists and the password provided matches
+    # the one we have password_digest for, everything's fine
     
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    # The Password.new returns an object that overrides the ==
+    # method. Instead of comparing two passwords directly
+    # (which is impossible because we only have a one-way hash)
+    # the == method calculates the candidate password_digest from
+    # the password given and compares it to the password_digest
+    # it was initialised with.
+    # So, to recap: THIS IS NOT A STRING COMPARISON 
+    else
+      nil
+    end
+  end
 end
