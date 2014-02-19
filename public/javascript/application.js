a6afeef17df20125b139b4d7bd739ae5aedd92b1
Jorja Hung <jorjahung@gmail.com>
2014-02-19 10:51:20 +0000

    setting 'favourited' value to star

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,10 +2,19 @@ $(function () {
 	console.log($("*").length);
 });
 
+// to make the star solid and hollow on click
 function addFavouritesHandler() {
 	$(".star.solid").click(function(event) {
-		var newOpacity = 1 - parseInt($(this).css('opacity'));
-		$(this).animate({opacity:newOpacity}, 500);
+		// get the link this star belongs to
+		var link = $(this).parent()
+		// get a booklean value for 'favourited'
+		// double negation casts any value to boolean
+		var favourited = !!$(link).data("favourited");
+		var newOpacity =favourited ? 0 : 1;
+		// toggle the "favouited" variable on the link element
+		$(link).data("favourited", !favourited);
+		//perform the animation
+		$(this).animate({opacity:newOpacity}, 1000);
 	});
 };
 
