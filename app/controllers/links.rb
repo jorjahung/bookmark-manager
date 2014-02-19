533a5fadfcab302e6c9cc219b26e24d5d989ac18
Jorja Hung <jorjahung@gmail.com>
2014-02-19 17:12:41 +0000

    responsive to phones

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -1,6 +1,7 @@
 post '/links' do
 	url = params['url']
 	title = params['title']
+	description = params['description']
 	tags = params['tags'].split(' ').map { |tag| Tag.first_or_create(:text => tag) }
 	Link.create(url: url, title: title, tags: tags)
 	redirect to ('/')
